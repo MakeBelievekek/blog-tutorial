ba7b10c5242bb02a396b34de580fb37072653666
Mate Toth (tom1lb) <tom1lb@bosch.com>
2022-10-17 15:13:44 +0200

    login page wip

--- src/app/login/feature/login/login.module.ts
+++ src/app/login/feature/login/login.module.ts
@@ -3,6 +3,9 @@ import {CommonModule} from '@angular/common';
 import {LoginPage} from "./login-page";
 import {LoginRoutingModule} from "./login-routing.module";
 import {LoginFormModule} from "../../sahred/ui/login-form/login-form.module";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
 
 
 @NgModule({
@@ -12,7 +15,10 @@ import {LoginFormModule} from "../../sahred/ui/login-form/login-form.module";
   imports: [
     CommonModule,
     LoginFormModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ]
 })
 export class LoginModule {
